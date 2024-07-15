a0261eba19d3164b644ebfc0b48f12c8e8075c64
MayHyeyeonKim <hkim7963@gmail.com>
2024-07-15 13:58:55 -0700

    feat: add completed thumbnail

--- src/components/GFLPopup.tsx
+++ src/components/GFLPopup.tsx
@@ -1,10 +1,10 @@
-import { Stack, Typography, Button, Box, Avatar } from '@mui/material';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import { PopupInfo } from '../models/popupInfo';
+import { Stack, Typography, Button, Box, Avatar } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { PopupInfo } from "../models/popupInfo";
 
 interface GFLPopupProps {
   popupInfo: PopupInfo | null;
@@ -13,7 +13,7 @@ interface GFLPopupProps {
 
 export const GFLPopup: React.FC<GFLPopupProps> = ({
   popupInfo = null,
-  onClose
+  onClose,
 }) => {
   if (!popupInfo) return null;
 
@@ -22,19 +22,27 @@ export const GFLPopup: React.FC<GFLPopupProps> = ({
   return (
     <Box
       sx={{
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        width: '100%',
-        backgroundColor: 'white',
-        boxShadow: '0 -2px 10px rgba(0, 0, 0, 0.1)',
-        borderRadius: '30px',
+        position: "fixed",
+        bottom: 10,
+        width: "60%",
+        backgroundColor: "white",
+        boxShadow: "0 -2px 10px rgba(0, 0, 0, 0.1)",
+        borderRadius: "15px",
         zIndex: 1000,
-        p: 2,
+        // p: 2,
+        mb: 2,
       }}
     >
-      <Box sx={{ width: '100%', display: 'flex', flexDirection: 'row' }}>
-        <Box sx={{ width: '40%', p: 1 }}>
+      <Box sx={{ width: "100%", display: "flex", flexDirection: "row",}}>
+        <Box
+          sx={{
+            width: "40%",
+            // p: 1,
+            justifyContent: "center",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
           {images && images.length > 0 && (
             <Carousel
               showThumbs={false}
@@ -42,92 +50,142 @@ export const GFLPopup: React.FC<GFLPopupProps> = ({
               showArrows={true}
               renderArrowPrev={(onClickHandler, hasPrev, label) =>
                 hasPrev && (
-                  <button type="button" onClick={onClickHandler} title={label} style={arrowPrevStyles}>
-                    <ArrowBackIosIcon />
+                  <button
+                    type="button"
+                    onClick={onClickHandler}
+                    title={label}
+                    style={arrowPrevStyles}
+                  >
+                    <ArrowBackIosIcon style={arrowIconStyles} />
                   </button>
                 )
               }
               renderArrowNext={(onClickHandler, hasNext, label) =>
                 hasNext && (
-                  <button type="button" onClick={onClickHandler} title={label} style={arrowNextStyles}>
-                    <ArrowForwardIosIcon />
+                  <button
+                    type="button"
+                    onClick={onClickHandler}
+                    title={label}
+                    style={arrowNextStyles}
+                  >
+                    <ArrowForwardIosIcon style={arrowIconStyles} />
                   </button>
                 )
               }
             >
               {images.map((image, index) => (
                 <div key={index}>
-                  <img src={image} alt={`${popupInfo.hospitalInfo.name} Picture`} style={{ width: '100%', height: 'auto' }} />
+                  <img
+                    src={image}
+                    alt={`${popupInfo.hospitalInfo.name} Picture`}
+                    style={{ width: "100%", height: "100%", borderTopLeftRadius: "15px", borderBottomLeftRadius: "15px" }}
+                  />
                 </div>
               ))}
             </Carousel>
           )}
         </Box>
-        <Box sx={{ width: '60%', p: 2, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+        <Box
+          sx={{
+            width: "60%",
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
           <Stack spacing={1}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
               <LocationOnIcon color="primary" />
               <Typography variant="body2" sx={{ ml: 1 }}>
                 {popupInfo.hospitalInfo.city}, {popupInfo.hospitalInfo.state}
               </Typography>
             </Box>
-            <Typography fontWeight={600}>{popupInfo.hospitalInfo.name}</Typography>
-            <Typography variant="body2" sx={{ fontStyle: 'italic' }}>
+            <Typography fontWeight={600}>
+              {popupInfo.hospitalInfo.name}
+            </Typography>
+            <Typography variant="body2" sx={{ fontStyle: "italic" }}>
               "{popupInfo.hospitalInfo.description}"
             </Typography>
           </Stack>
-          <Button variant="contained" color="primary" onClick={onClose}>Learn more</Button>
+          <Button variant="contained" color="primary" onClick={onClose}>
+            Learn more
+          </Button>
         </Box>
-        <Box sx={{ width: '30%', p: 2, borderLeft: '1px solid #ddd', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+        <Box
+          sx={{
+            width: "30%",
+            p: 2,
+            borderLeft: "1px solid #ddd",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
           <Stack spacing={2}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Avatar src="/path/to/profile1.jpg" sx={{ width: 24, height: 24, mr: 1 }} />
-              <Avatar src="/path/to/profile2.jpg" sx={{ width: 24, height: 24, mr: 1 }} />
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Avatar
+                src="/path/to/profile1.jpg"
+                sx={{ width: 24, height: 24, mr: 1 }}
+              />
+              <Avatar
+                src="/path/to/profile2.jpg"
+                sx={{ width: 24, height: 24, mr: 1 }}
+              />
               <Avatar sx={{ width: 24, height: 24 }}>+</Avatar>
             </Box>
             <Typography variant="body2">
               Donations received: <strong>150k</strong>
             </Typography>
-            <Typography variant="body2">
-              400+ kids impacted
-            </Typography>
+            <Typography variant="body2">400+ kids impacted</Typography>
           </Stack>
+          <Button variant="contained" color="primary" onClick={onClose}>
+            Donate
+          </Button>
         </Box>
+        <Button
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+          }}
+        >
+          X
+        </Button>
       </Box>
     </Box>
   );
 };
 
-const arrowPrevStyles = {
+const arrowButtonStyles: React.CSSProperties = {
   position: 'absolute',
   zIndex: 2,
-  top: 'calc(50% - 15px)',
-  left: 15,
-  width: 30,
-  height: 30,
-  cursor: 'pointer',
-  backgroundColor: 'rgba(255, 255, 255, 0.8)',
+  top: 'calc(50% - 10px)', // 크기를 줄인 만큼 위치 조정
+  background: 'none',
   border: 'none',
+  color: 'white',
+  cursor: 'pointer',
+  padding: 0,
+  margin: 0,
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  borderRadius: '50%',
-  boxShadow: '0 2px 5px rgba(0, 0, 0, 0.3)',
+  width: '30px', // 크기 조정
+  height: '30px', // 크기 조정
+  outline: 'none', // 클릭 시 테두리 제거
+};
+
+const arrowPrevStyles = {
+  ...arrowButtonStyles,
+  left: 10,
 };
 
 const arrowNextStyles = {
-  position: 'absolute',
-  zIndex: 2,
-  top: 'calc(50% - 15px)',
-  right: 15,
-  width: 30,
-  height: 30,
-  cursor: 'pointer',
-  backgroundColor: 'rgba(255, 255, 255, 0.1)',
-  border: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  borderRadius: '50%',
-  boxShadow: '0 2px 5px rgba(0, 0, 0, 0.3)',
+  ...arrowButtonStyles,
+  right: 10,
+};
+
+const arrowIconStyles: React.CSSProperties = {
+  fontSize: '20px', // 아이콘 크기를 줄임
 };
