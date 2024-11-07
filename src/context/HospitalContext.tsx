50eea6f47f1e5182dcb17f0cba340e87e40f7a96
MayHyeyeonKim <hkim7963@gmail.com>
2024-11-07 02:30:06 -0800

    working on the HospitalCard popup

--- src/context/HospitalContext.tsx
+++ src/context/HospitalContext.tsx
@@ -30,6 +30,7 @@ export const HospitalsContextProvider = (props: { children: ReactNode }) => {
   const [hospitals, setHospitals] = useState<Hospital[] | undefined>(undefined);
 
   useEffect(() => {
+    console.log("hospital있어?", hospitals);
     setHospitals(originals);
   }, [originals]);
 
